1362a11405b3ec2ddc2adef7afa1ee675043491d
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-06-16 16:14:06 +0530

    Create generate_page function

---
+++ src/generate_page.py
@@ -0,0 +1,24 @@
+from markdown_to_html_node import mark_down_to_html_node
+from extract_title import extract_title
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    file = open(from_path)
+    markdown = file.read()
+    file.close()
+
+    file = open(template_path)
+    template = file.read()
+    file.close()
+
+    content = mark_down_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", content)
+
+    file = open(dest_path, "w")
+    file.write(template)
+    file.close()
